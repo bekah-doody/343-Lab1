feea1a922e7ebb521de7b82cb8b4831b3f01f730
Bekah Doody <doodyr@mail.gvu.edu>
2024-09-15 13:47:49 -0400

    rough draft of all methods in .c file

--- lite_vector.c
+++ lite_vector.c
@@ -1,21 +1,48 @@
-#include "lite_vector.h"
+#include "lite_vreturn true;ector.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
+//must go through and add error handling (malloc failures)
+
 lite_vector* lv_new_vec(size_t type_size){
+    lite_vector* vec = (lite_vector*)malloc(sizeof(lite_vector));
+    vec->length = 0;
+    vec->max_capacity = 4;
+    vec->type_size = type_size;
+    vec->data = (void**)malloc(vec->max_capacity * sizeof(void*));
+    //find out how to check if didn't work so you can return NULL
+    return vec;
+
+
 }
 
 void lv_cleanup(lite_vector* vec){
+    //must free every element in vec->data, then the array, then vec
+    for (int i = 0; i < vec->length; i++) {
+        free(vec->data[i]);
+     }
+    free(vec->data);
+    free(vec);
 }
 
 size_t lv_get_length(lite_vector* vec){
+    return vec->length;
+
 }
 
 bool lv_clear(lite_vector* vec){
+    for (int i = 0; i < vec->length; i++) {
+        free(vec->data[i]);
+    }
+    vec->length = 0;
+    return true;
+    //figure out false case
 }
 
 void* lv_get(lite_vector* vec, size_t index){
+    return vec->data[index];
+    
 }
 
 /**
@@ -29,7 +56,21 @@ void* lv_get(lite_vector* vec, size_t index){
  * must remain unaffected.
  */
 static bool lv_resize(lite_vector* vec){
+    size_t resize_capacity = vec->max_capacity * 2;
+    void** resize_data = (void**)realloc(vec->data, resize_capacity * sizeof(void*));
+    //check if memory allocation worked here, return false if didnt
+    vec->data = resize_data;
+    vec->max_capacity = resize_capacity;
+    return true;
 }
 
 bool lv_append(lite_vector* vec, void* element){
+    if (vec->length + 1 >= vec->max_capacity) {
+        //possibly call resize?
+        return false;
+    }
+    vec->data[vec->length] = element;
+    vec->length = vec->length + 1;
+    return true;
 }
+
