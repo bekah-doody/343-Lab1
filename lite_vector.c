138770a590225cf339aabae563a3ac37e06e5025
Bekah Doody <doodyr@mail.gvu.edu>
2024-09-17 18:41:23 -0400

    added in error handling and commented code a lil

--- lite_vector.c
+++ lite_vector.c
@@ -7,17 +7,30 @@
 
 lite_vector* lv_new_vec(size_t type_size){
     lite_vector* vec = (lite_vector*)malloc(sizeof(lite_vector));
+    //check if allocation didn't work
+    if (vec == NULL) {
+        return NULL; 
+    }
+
     vec->length = 0;
     vec->max_capacity = 4;
     vec->type_size = type_size;
     vec->data = (void**)malloc(vec->max_capacity * sizeof(void*));
-    //find out how to check if didn't work so you can return NULL
+    //check if allocation didn't work
+    if (vec->data == NULL) {
+        free(vec);
+        return NULL;
+    }
     return vec;
 
 
 }
 
 void lv_cleanup(lite_vector* vec){
+    //check if cev exists to cleanup
+    if (vec == NULL) {
+        return;
+    }
     //must free every element in vec->data, then the array, then vec
     for (int i = 0; i < vec->length; i++) {
         free(vec->data[i]);
@@ -27,20 +40,35 @@ void lv_cleanup(lite_vector* vec){
 }
 
 size_t lv_get_length(lite_vector* vec){
+    //check if vec exists to get length of
+    if (vec == NULL) {
+        return 0; //length DNE
+    }
     return vec->length;
 
 }
 
 bool lv_clear(lite_vector* vec){
+    //check if vec existst to clear
+    if (vec == NULL) {
+        return false;
+    }
     for (int i = 0; i < vec->length; i++) {
         free(vec->data[i]);
     }
     vec->length = 0;
     return true;
-    //figure out false case
 }
 
 void* lv_get(lite_vector* vec, size_t index){
+    //check if vec exists to get
+    if (vec == NULL) {
+        return NULL;
+    }
+    //check if invalid index
+    if (vec->length >= index) {
+        return NULL;
+    }
     return vec->data[index];
     
 }
@@ -56,18 +84,32 @@ void* lv_get(lite_vector* vec, size_t index){
  * must remain unaffected.
  */
 static bool lv_resize(lite_vector* vec){
+    //check if vec exists to resize
+    if (vec == NULL) {
+        return false;
+    }
     size_t resize_capacity = vec->max_capacity * 2;
     void** resize_data = (void**)realloc(vec->data, resize_capacity * sizeof(void*));
-    //check if memory allocation worked here, return false if didnt
+    //check if memory alloc didn't work
+    if (resize_data = NULL) {
+        return false;
+    }
     vec->data = resize_data;
     vec->max_capacity = resize_capacity;
     return true;
 }
 
 bool lv_append(lite_vector* vec, void* element){
+    //check if vec exists to append to
+    if (vec == NULL) {
+        return false;
+    }
     if (vec->length + 1 >= vec->max_capacity) {
         //possibly call resize?
-        return false;
+        bool resized = lv_resize(vec);
+        if (resized != true) {
+            return false;
+        }
     }
     vec->data[vec->length] = element;
     vec->length = vec->length + 1;
